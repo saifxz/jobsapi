7d5e77745349fd087df1c831b958ce24a64a71a5
saif <saiffuddin9@gmail.com>
2023-12-31 18:40:13 +0530

    second commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connectDB=require('./db/connect')
 const helmet=require('helmet')
 const cors=require('cors')
 const xss=require('xss-clean')
-const rateLimiter=require('rate-limiter')
+// const rateLimiter=require('rate-limiter')
 
 const express = require('express');
 const app = express();
@@ -22,13 +22,13 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
-app.use(rateLimiter({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Use an external store for consistency across multiple server instances.
-}))
+// app.use(rateLimiter({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	// store: ... , // Use an external store for consistency across multiple server instances.
+// }))
 app.use(express.json());
 // extra packages
 app.use(cors())
